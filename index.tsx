fbedf5e3800604754cd54f6a04c7fdd06113b00e
umergenerat <aitloutouaom@gmail.com>
2026-02-08 13:47:42 +0100

    feat: Add PWA support by registering a service worker and including necessary icons and type definitions.

--- index.tsx
+++ index.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { registerSW } from 'virtual:pwa-register';
+
+// Register Service Worker for PWA
+registerSW({ immediate: true });
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error("Could not find root element to mount to");
